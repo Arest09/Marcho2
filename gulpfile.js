388dc34d0748476080016af1c714755ec1ee7e30
arest <arest09@mail.ru>
2022-02-12 15:53:03 +1000

    video fashion

--- gulpfile.js
+++ gulpfile.js
@@ -23,7 +23,7 @@ function browsersync() {
 }
 
 function styles() {
-    return src('app/scss/style.scss')
+    return src(['app/scss/style.scss'])
         .pipe(scss({
             outputStyle: 'compressed'
         }))
@@ -83,6 +83,7 @@ function images() {
 function scripts() {
     return src(['node_modules/jquery/dist/jquery.js',
             'node_modules/slick-carousel/slick/slick.js',
+            'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
             'app/js/main.js'
         ])
         .pipe(concat('main.min.js'))
